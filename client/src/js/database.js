9cdcef62e6fcb4b9761eb9b5ef31d8ffb7a9ae7b
rak10 <ahmadus10@icloud.com>
2023-03-22 01:00:29 +0500

    added css and logic files for client

---
+++ client/src/js/database.js
@@ -0,0 +1,40 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log(' database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+  const request = store.put({  jate: content });
+  
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
+
+export const getDb = async () => {
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+  
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  
+}
+
+initdb();
